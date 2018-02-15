cfc98853784dbf82ebabd6000a0fd43af1af5727
thomas.every <thomas.every@northumbria.ac.uk>
2018-02-15 10:59:33 +0000

    Unfinished potentiometer monitor with representative bars

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,24 @@ C12832 lcd(D11, D13, D12, D7, D10);
 
 int main()
 {
-    int counter = 0;
-    
-    lcd.cls();
-    lcd.rect(0,0,127,31,1);
-    lcd.locate(2,3);
-    lcd.printf("LCD basics");
-    while(true) {   // this is the third thread
-      lcd.locate(2,12);
-      lcd.printf("counter: %09d", counter);
-      counter += 1;
-      wait_ms(200);
-    }
+  AnalogIn leftPot(A0);
+  AnalogIn rightPot(A1);
+
+  lcd.cls();
+  lcd.rect(0,0,127,31,1);
+  lcd.locate(2,3);
+  lcd.printf("Potentiometer monitor");
+  while(true) {   // this is the third thread
+    lcd.locate(2,12);
+    lcd.printf("LPot %1.2f", leftPot.read());
+    double lPot  = leftPot.read();
+    int lPotPercent = ((lPot/80)*1000)+45;
+    lcd.rect(45,12,lPotPercent,20,1);
+
+    lcd.locate(2,21);
+    lcd.printf("RPot %1.2f", rightPot.read());
+    lcd.rect(45,21,125,29,1);
+    wait_ms(200);
+  }
+
 }
